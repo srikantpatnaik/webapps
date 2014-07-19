e0ef18b1e62937283b5c4c813885805283872e2c
Srikant Patnaik <srikant@gnu-linux.org>
2014-07-19 19:40:17 +0530

    a version with filetype check

--- convert/models/db.py
+++ convert/models/db.py
@@ -2,9 +2,11 @@
 
 db = DAL('mysql://srikant:homeauto@localhost/ha_db')
 
-dropdown = ('jpg', 'svg', 'png', 'doc')
+dropdown = ('jpg', 'pdf', 'png', 'doc')
+possible_extensions = ('jpg', 'pdf', 'png', 'doc')
 
 db.define_table('converter',
-                Field('upload_input_file', 'upload', uploadfolder = request.folder+'uploads', requires=IS_NOT_EMPTY(), autodelete=True),
+                Field('upload_input_file', 'upload', uploadfolder = request.folder+'uploads',
+                      requires = IS_UPLOAD_FILENAME(extension='png'), autodelete=True),
                 Field('convert_to', requires = IS_IN_SET(dropdown), default = dropdown[0]),
                )
